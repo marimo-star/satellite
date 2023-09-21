910a90823323335f82b0ceabd6f3a2febca680fe
marimo-star <144980938+marimo-star@users.noreply.github.com>
2023-09-21 05:05:59 +0000

    あああああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
 # Streamlitアプリのタイトル
-st.title("衛星の軌道計算アプリ")
+st.title("宇宙デブリの軌道計算アプリ")
 
-# 衛星の初期位置（x, y, z） [m]
-initial_position_x = st.number_input("初期X座標 [m]", value=700000)
-initial_position_y = st.number_input("初期Y座標 [m]", value=0)
+# 宇宙デブリの初期位置（x, y, z） [m]
+initial_position_x = st.number_input("初期X座標 [m]", value=100000)
+initial_position_y = st.number_input("初期Y座標 [m]", value=100000)
 initial_position_z = st.number_input("初期Z座標 [m]", value=0)
 
-# 衛星の初期速度（vx, vy, vz） [m/s]
-initial_velocity_x = st.number_input("初期X速度 [m/s]", value=0)
-initial_velocity_y = st.number_input("初期Y速度 [m/s]", value=7500)
+# 宇宙デブリの初期速度（vx, vy, vz） [m/s]
+initial_velocity_x = st.number_input("初期X速度 [m/s]", value=1000)
+initial_velocity_y = st.number_input("初期Y速度 [m/s]", value=0)
 initial_velocity_z = st.number_input("初期Z速度 [m/s]", value=0)
 
 # シミュレーションの時間範囲
@@ -22,26 +22,26 @@ simulation_time = st.number_input("シミュレーション時間（秒）", val
 # 地球の質量 [kg]
 earth_mass = 5.972e24
 
-# 衛星の質量 [kg]
-satellite_mass = 1000
+# 宇宙デブリの質量 [kg]（小さなデブリの場合、無視することが一般的）
+debris_mass = 1.0
 
-def satellite_motion(t, state):
+def debris_motion(t, state):
     x, y, z, vx, vy, vz = state
     r = np.sqrt(x**2 + y**2 + z**2)
     gravitational_force = -earth_mass / r**3 * np.array([x, y, z])
     dxdt = vx
     dydt = vy
     dzdt = vz
-    dvxdt = gravitational_force[0] / satellite_mass
-    dvydt = gravitational_force[1] / satellite_mass
-    dvzdt = gravitational_force[2] / satellite_mass
+    dvxdt = gravitational_force[0] / debris_mass
+    dvydt = gravitational_force[1] / debris_mass
+    dvzdt = gravitational_force[2] / debris_mass
     return [dxdt, dydt, dzdt, dvxdt, dvydt, dvzdt]
 
 initial_state = [initial_position_x, initial_position_y, initial_position_z,
                  initial_velocity_x, initial_velocity_y, initial_velocity_z]
 
 # 解を計算
-solution = solve_ivp(satellite_motion, (0, simulation_time), initial_state, t_eval=np.linspace(0, simulation_time, 1000))
+solution = solve_ivp(debris_motion, (0, simulation_time), initial_state, t_eval=np.linspace(0, simulation_time, 1000))
 
 # 結果をプロット
 fig = plt.figure(figsize=(10, 6))
@@ -50,6 +50,5 @@ ax.plot(solution.y[0], solution.y[1], solution.y[2])
 ax.set_xlabel('X軸 [m]')
 ax.set_ylabel('Y軸 [m]')
 ax.set_zlabel('Z軸 [m]')
-ax.set_title('衛星の軌道')
+ax.set_title('宇宙デブリの軌道')
 st.pyplot(fig)
-
